952e0ddae062612d27c39832bd1c4f5ed010fb98
Александр Биктеев <bikteev@mail.ru>
2022-11-25 08:35:11 +0600

    Initial Commit

---
+++ TrafficLightUsingSUI/TrafficLightUsingSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightUsingSUIApp.swift
+//  TrafficLightUsingSUI
+//
+//  Created by albik on 25.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightUsingSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
